12edebe8be15db5f0e4115f4d804120c5ca996c6
Rob Rix <rob.rix@me.com>
2021-08-13 11:47:28 -0400

    Define foldMap/fmap in terms of traverse.

--- src/Effect/Syntax.hs
+++ src/Effect/Syntax.hs
@@ -29,10 +29,10 @@ class Syn rep where
 newtype a & b = With { getWith :: forall r . Either (r • a) (r • b) -> r }
 
 instance Foldable ((&) a) where
-  foldMap = bifoldMap (const mempty)
+  foldMap = foldMapDefault
 
 instance Functor ((&) a) where
-  fmap = second
+  fmap = fmapDefault
 
 instance Traversable ((&) a) where
   traverse = bitraverse pure
