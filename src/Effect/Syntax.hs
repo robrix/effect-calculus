0bb9283b93c69c4a0913a645dec1c1cf84cbbba0
Rob Rix <rob.rix@me.com>
2021-08-13 11:27:03 -0400

    Define a With type.

--- src/Effect/Syntax.hs
+++ src/Effect/Syntax.hs
@@ -1,6 +1,13 @@
 module Effect.Syntax
 ( Syn(..)
+  -- * Connectives
+, type (&)(..)
 ) where
 
 class Syn rep where
   hdl :: (msg -> rep a) -> rep a
+
+
+-- Connectives
+
+newtype a & b = With { getWith :: forall x . (Either (a -> x) (b -> x) -> x) -> x }
