15d4715432e2ee49a9bf701db434b7baac6c91ef
Rob Rix <rob.rix@me.com>
2021-08-13 11:37:58 -0400

    ⊕ has strict fields.

--- src/Effect/Syntax.hs
+++ src/Effect/Syntax.hs
@@ -26,7 +26,7 @@ newtype a & b = With { getWith :: forall x . Either (a -> x) (b -> x) -> x }
 instance Bifunctor (&) where
   bimap f g r = With (getWith r . bimap (. f) (. g))
 
-data a ⊕ b = L a | R b
+data a ⊕ b = L !a | R !b
 
 instance Foldable ((⊕) a) where
   foldMap = bifoldMap (const mempty)
